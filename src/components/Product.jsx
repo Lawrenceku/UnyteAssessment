f0e54efd3edbb98585aede056811bebd3270e8ac
Lawrence Eniola <lawrencekunmilola@gmail.com>
2024-03-17 23:42:02 +0100

    search feature finished

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,10 +1,10 @@
-/* import React from "react";
+import React from "react";
 
 const Product = ({ product }) => {
   return (
-    <div className="m-8 relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+    <div className="m-8 relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md ">
       <div className="relative mx-4 mt-4 h-96 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700">
-        <img src={product.image} className="h-full w-full object-cover" alt={product.name} />
+        <img src={product.image} className="h-full w-full object-cover hover:scale-110 transition-transform duration-300" alt={product.name} />
       </div>
       <div className="p-6">
         <div className="mb-2 flex items-center justify-between">
@@ -21,7 +21,7 @@ const Product = ({ product }) => {
       </div>
       <div className="p-6 pt-0">
         <button
-          className="block w-full select-none rounded-lg bg-blue-gray-200 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          className="block w-full select-none rounded-lg bg-gray-200 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
           type="button"
         >
           Compare
@@ -32,4 +32,3 @@ const Product = ({ product }) => {
 };
 
 export default Product;
- */
--- src/products.js
+++ src/products.js
@@ -6,6 +6,7 @@ export const products = [
       description:
         "With plenty of talk and listen time, voice-activated Siri access, and an available wireless charging case.",
       image: '//images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=927&amp;q=80',
+      category:['electronics', 'music']
     },
     {
       id: 2,
@@ -14,14 +15,16 @@ export const products = [
       description:
         "Unisex Baseball Face Cap High Quality   Fashionable   Available color: Black   Weight: 0.2KG",
       image: 'https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/43/4220191/1.jpg?9380',
+      category:['fashion', 'cloth']
     },
     {
       id: 3,
       name: "Big Face Men's Watch Waterproof Quartz Wristwatch- Black",
-      price: "$2.99",
+      price: "$4.99",
       description:
         "Jazz up your outfits and add sophistication to your look with this exquisite watch that has been specially designed for that personality of class and style.",
       image: 'https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/71/3968902/1.jpg?6208',
+      category:['electronics', 'smartwatch']
     },
     {
       id: 4,
@@ -30,6 +33,7 @@ export const products = [
       description:
         "Engineered by SpaceX, Starlink is designed to deliver high-speed, low-latency internet to the most rural and remote locations around the world. ",
       image: 'https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/21/3439362/1.jpg?6192',
+      category:['electronics', 'tech', 'internet services']
     },
     {
       id: 5,
@@ -38,6 +42,7 @@ export const products = [
       description:
         "Pro camera system with new 12MP Telephoto, Wide, and Ultra Wide cameras; LiDAR Scanner; 6x optical zoom range; macro photography; Photographic Styles, ProRes video, Smart HDR 4, Night mode, Apple ProRAW, 4K Dolby Vision HDR recording",
       image: 'https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/42/7052472/1.jpg?8790',
+      category:['electronics', 'Smartphones']
     },
     {
       id: 6,
@@ -46,6 +51,7 @@ export const products = [
       description:
         "The concept is designed to suit the very desire of having most of our everyday motivations in simple, handy, convenient, comfortable, and easy-to-handle wear. It's capable of bringing out a lot of vibe in fact, all-in-one.",
       image: 'https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/82/183283/1.jpg?1710',
+      category:['fashion', 'cloth']
     },
     {
       id: 7,
@@ -54,6 +60,7 @@ export const products = [
       description:
         "The sleek design and soft, elastic insole provide exceptional comfort and support, offering a superior wearing experience.",
       image: 'https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/80/2526372/1.jpg?0124https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/80/2526372/1.jpg?0124',
+      category:['fashion', 'cloth', 'shoe', 'footwear']
     }, 
       {
       id: 8,
@@ -62,6 +69,7 @@ export const products = [
       description:
         "Immerse yourself in the world of entertainment with the Itel 32 Inches LED TV (A3250). This state-of-the-art LED TV combines cutting-edge technology with a sleek and modern design to provide an unparalleled viewing experience.",
       image: 'https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/94/5031362/1.jpg?0002',
+      category:['electronics', 'tv', 'home appliances']
     },
     {
       id: 9,
@@ -70,6 +78,7 @@ export const products = [
       description:
         "Easily Create Anything From Dips To Drinks, Doughs To Desserts With This Next-Generation Ninja Food Processor, Multi-Serve Blender And Personal Blender In One, Complete With Interchangeable Blades And Attachments.",
       image: 'https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/96/714768/1.jpg?1948',
+      category:['electronics', 'food', 'culinary','home appliances']
     },
     {
       id: 10,
@@ -78,6 +87,7 @@ export const products = [
       description:
         "This versatile T-Shirt is the best fit for your daily grind, a weekend get together or that upcoming date. It is made from comfortable and durable cotton fabric.",
       image: 'https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/34/267579/1.jpg?0879',
+      category:['fashion', 'cloth']
     },
    
   ];
