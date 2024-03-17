a5bac09e59c403ad54834e3e70c64f3ca28c8db8
Lawrence Eniola <lawrencekunmilola@gmail.com>
2024-03-17 21:12:50 +0100

    mock products

--- src/products.js
+++ src/products.js
@@ -9,14 +9,22 @@ export const products = [
     },
     {
       id: 2,
-      name: "itel 32 Inches LED TV (A3250) ",
-      price: "$56.99",
+      name: "Unisex Gridding Sports Baseball Face Cap - Black",
+      price: "$2.99",
       description:
-        "Immerse yourself in the world of entertainment with the Itel 32 Inches LED TV (A3250). This state-of-the-art LED TV combines cutting-edge technology with a sleek and modern design to provide an unparalleled viewing experience.",
-      image: 'https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/94/5031362/1.jpg?0002',
+        "Unisex Baseball Face Cap High Quality   Fashionable   Available color: Black   Weight: 0.2KG",
+      image: 'https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/43/4220191/1.jpg?9380',
     },
     {
       id: 3,
+      name: "Big Face Men's Watch Waterproof Quartz Wristwatch- Black",
+      price: "$2.99",
+      description:
+        "Jazz up your outfits and add sophistication to your look with this exquisite watch that has been specially designed for that personality of class and style.",
+      image: 'https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/71/3968902/1.jpg?6208',
+    },
+    {
+      id: 4,
       name: "Starlink Standard Kit",
       price: "$504.99",
       description:
@@ -24,7 +32,7 @@ export const products = [
       image: 'https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/21/3439362/1.jpg?6192',
     },
     {
-      id: 4,
+      id: 5,
       name: "Apple IPhone 13 Pro Max - 6.7'' - 6GB RAM, 128GB ROM, 5G - Blue",
       price: "$699.99",
       description:
@@ -32,7 +40,7 @@ export const products = [
       image: 'https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/42/7052472/1.jpg?8790',
     },
     {
-      id: 5,
+      id: 6,
       name: "Men Lightweight Winter Varsity Jacket - White",
       price: "$3.99",
       description:
@@ -40,19 +48,36 @@ export const products = [
       image: 'https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/82/183283/1.jpg?1710',
     },
     {
-      id: 6,
+      id: 7,
       name: "ASHION Casual Sneakers: Stylish Suede Design For Superior Comfort And Durabilit",
       price: "$8.99",
       description:
-        "With plenty of talk and listen time, voice-activated Siri access, and an available wireless charging case.",
+        "The sleek design and soft, elastic insole provide exceptional comfort and support, offering a superior wearing experience.",
       image: 'https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/80/2526372/1.jpg?0124https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/80/2526372/1.jpg?0124',
+    }, 
+      {
+      id: 8,
+      name: "itel 32 Inches LED TV (A3250) ",
+      price: "$56.99",
+      description:
+        "Immerse yourself in the world of entertainment with the Itel 32 Inches LED TV (A3250). This state-of-the-art LED TV combines cutting-edge technology with a sleek and modern design to provide an unparalleled viewing experience.",
+      image: 'https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/94/5031362/1.jpg?0002',
     },
     {
-      id: 7,
-      name: "Unisex Gridding Sports Baseball Face Cap - Black",
-      price: "$2.99",
+      id: 9,
+      name: "Ninja 3 IN 1 FOOD PROCESSOR AND BLENDER",
+      price: "$170.99",
       description:
-        "Unisex Baseball Face Cap High Quality   Fashionable   Available color: Black   Weight: 0.2KG",
-      image: 'https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/43/4220191/1.jpg?9380',
-    }
+        "Easily Create Anything From Dips To Drinks, Doughs To Desserts With This Next-Generation Ninja Food Processor, Multi-Serve Blender And Personal Blender In One, Complete With Interchangeable Blades And Attachments.",
+      image: 'https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/96/714768/1.jpg?1948',
+    },
+    {
+      id: 10,
+      name: "Plain Mens T-shirts Combo Of 4",
+      price: "$5.00",
+      description:
+        "This versatile T-Shirt is the best fit for your daily grind, a weekend get together or that upcoming date. It is made from comfortable and durable cotton fabric.",
+      image: 'https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/34/267579/1.jpg?0879',
+    },
+   
   ];
