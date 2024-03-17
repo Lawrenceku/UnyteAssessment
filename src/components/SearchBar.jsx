85ce6f6264f82571187af42883f422290d6c42a1
Lawrence Eniola <lawrencekunmilola@gmail.com>
2024-03-17 20:47:33 +0100

    search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -34,6 +34,7 @@ const Product = ({ product }) => {
   };
 
 const SearchBar = () => {
+  const [inputSearch, setInputSearch] = useState('')
     const [searchItem, setSearchItem] = useState('')
     return (
         <>
@@ -44,12 +45,12 @@ const SearchBar = () => {
                         <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                             <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                         </div>
-                        <input onChange={(event)=>setSearchItem(event.target.value)} type="search" id="default-search" className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-unyteGreen focus:border-[#5CC048] dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search products..." required />
-                        <button  className="text-white absolute right-2.5 bottom-2.5 bg-unyteGreen hover:bg-[#5CC048] focus:ring-4 focus:outline-none focus:ring-[#5CCf98] font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                        <input onChange={(event)=>setInputSearch(event.target.value)}  type="search" id="default-search" className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-unyteGreen focus:border-[#5CC048] dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search products..." required />
+                        <button onClick={(event)=>setSearchItem(inputSearch)}  className="text-white absolute right-2.5 bottom-2.5 bg-unyteGreen hover:bg-[#5CC048] focus:ring-4 focus:outline-none focus:ring-[#5CCf98] font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
                     </div>
             </div>
             </div>
-            <div className="flex md:flex-row flex-wrap justify-center items-baseline md:items-start md:justify-between items">
+            <div className="flex flex-row flex-wrap justify-center md:items-start md:justify-between items">
                {
                 products.filter((product) =>{
                     if(searchItem ==''){
