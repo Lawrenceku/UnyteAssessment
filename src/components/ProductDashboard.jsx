124b67485116c9f808fbbdd8ae2ad6e86f202a96
Lawrence Eniola <lawrencekunmilola@gmail.com>
2024-03-25 10:50:23 +0100

    sccroll fix

--- src/components/ProductDashboard.jsx
+++ src/components/ProductDashboard.jsx
@@ -65,7 +65,7 @@ const ProductDashboard = () => {
 <div id="container" className="fixed inset-0  flex justify-center items-center z-20">
     <div className="absolute inset-0 bg-gray-900 opacity-90  z-10"></div>
 
-    <div className="relative z-10 w-full overflow-scroll">
+    <div className="relative z-10 w-full overflow-auto">
         <div className="flex justify-center items-start relative bg-white">
             {selectedProducts.map((product) => (
                 <ProductComparison key={product.id} product={product} />
